1f58e478fd8b9a1c30d164eec118b12b730ccebe
sofivillanueva <acostavillanuevasofia@gmail.com>
2024-10-17 21:45:25 -0300

    Precerence migration model

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -5,4 +5,15 @@ class PreferencesController < ApplicationController
     @preferences = current_user.preferences
     @pagy, @records = pagy(@preferences)
   end
+
+  def create
+    @preference = PreferenceForm.new(args: user_form_params)
+
+    if @user.valid?
+      @user.save!
+      redirect_to users_path, notice: t('views.users.create_success')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
---
+++ spec/factories/preferences.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :preference do
+    name { "MyString" }
+    description { "MyText" }
+    restriction { false }
+  end
+end
