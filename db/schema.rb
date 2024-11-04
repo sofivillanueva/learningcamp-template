bb5fe8a91d4242cc3f7554e8deae1f89b2de7a54
Sebastián Caraballo <sebastian.caraballo@rootstrap.com>
2024-11-04 16:29:58 -0300

    final touches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_27_003430) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_20_213506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,7 +95,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_27_003430) do
   create_table "preferences", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.boolean "restriction"
+    t.boolean "restriction", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
