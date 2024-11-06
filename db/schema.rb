74236f155c25dc74e12f8307c10513e9f8adba54
sofivillanueva <acostavillanuevasofia@gmail.com>
2024-11-06 14:16:47 -0300

    Guardo cambios antes de merge

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_31_163656) do
   create_table "preferences", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.boolean "restriction"
+    t.boolean "restriction", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
