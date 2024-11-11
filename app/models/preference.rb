840553abddda60df21bc21b94f11394c67efe84c
sofivillanueva <acostavillanuevasofia@gmail.com>
2024-11-11 20:16:58 -0300

    i18n corrected

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -36,7 +36,7 @@
 
 # TASK: Ensure that the name, description, and restriction attributes are correctly handled in the preferences table.
 class Preference < ApplicationRecord
-  MAX_PREFERENCES = 5 
+  MAX_PREFERENCES = 5
   # validaciones
   validates :name, presence: true # no puede ser nulo
   validates :description, presence: true # no puede ser nulo
